f29bdc8eab95272812109db5bef451f3f6fb4948
Stephan Kulow <coolo@suse.de>
2020-09-10 07:05:49 +0200

    Make prg-brp-symlink compile on SLE15
    Gcc 7 does not have c++17 (but kindly ignores the command line to enable it),
    so we need a fallback header - newer compilers will still use the builtin
    implementation.
    
    The background is that we're using the Factory version of brp-check-suse
    for carwos

--- prg-brp-symlink/main.cpp
+++ prg-brp-symlink/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
-#include <filesystem>
 
-using namespace std;
+#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include) && __has_include(<filesystem>)
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#endif
 
 // synopsis: read from stdin lines per link
 // IFS=| link link_dest
@@ -13,23 +18,23 @@ using namespace std;
 // NOTE:
 // the actual file system content is of no concern here
 
-void check_link(const string &link, const string &link_dest) {
-   std::filesystem::path link_path(link);
-   std::filesystem::path link_dest_path = link_path.parent_path().append(link_dest);
-   std::filesystem::path link_dest_abs(link_dest_path.lexically_normal().string());
-   cout << link << "|"
+void check_link(const std::string &link, const std::string &link_dest) {
+   fs::path link_path(link);
+   fs::path link_dest_path = link_path.parent_path().append(link_dest);
+   fs::path link_dest_abs(link_dest_path.lexically_normal().string());
+   std::cout << link << "|"
         << link_dest << "|"
         << link_dest_abs.lexically_relative(link_path.parent_path()).string() << "|"
-        << link_dest_abs.string() << endl;
+        << link_dest_abs.string() << std::endl;
 }
 
-void work_line(const string &line) {
+void work_line(const std::string &line) {
     size_t delim = line.find('|');
     check_link("/" + line.substr(0, delim), line.substr(delim+1));
 }
 
 int main(int argc, char **argv) {
-    for (string line; std::getline(cin, line);) {
+    for (std::string line; std::getline(std::cin, line);) {
       work_line(line);
     }
 
